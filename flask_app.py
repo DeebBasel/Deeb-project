415f2e4f3b7b958e367b540683abb851c1e9f699
DeebBasel <deeb.basel@gmail.com>
2023-11-06 10:03:16 +0200

    naseem function is corrected

--- flask_app.py
+++ flask_app.py
@@ -13,7 +13,16 @@ app = Flask(__name__, template_folder='templateFiles', static_folder='staticFile
 # def welcome():
 #     return "This is the home page of Flask Application"
 def add(num1, num2):
-        return num1 + num2
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        result = num1 + num2
+    except:
+        print('Error: input values must be numbers')
+        result = None
+    return result
+
+
 
 @app.route('/')
 def home():
@@ -29,9 +38,18 @@ def fadi():
 
 @app.route('/naseem', methods=['GET','POST'])
 def naseem():
-    num1 = request.form.get('num1', type=int)
-    num2 = request.form.get('num2', type=int)
-    return add(num1, num2)
+    if request.method == 'GET':
+        return render_template('get_numbers.html')
+
+    num1 = request.form.get('num1')
+    num2 = request.form.get('num2')
+    result = add(num1, num2)
+    if result is None:
+        return "Error occured , please check the log"
+    else:
+        return f"The sum is {result}"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
